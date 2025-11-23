f62adc9d3c9b9d911f13dd5a4d03b1f5922e895b
lethargii <l.sevault@hotmail.com>
2025-11-23 08:32:37 +0100

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias pi="sudo pacman -Syu"
+alias ps="sudo pacman -Ss"
+alias pr="sudo pacman -Rs"
+source "$HOME/.cargo/env.fish"
+cat ~/.cache/wal/sequences
